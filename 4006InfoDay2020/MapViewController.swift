1f17990904d46a6a219f22c12be4bd20097c2af3
wcy1216 <https://github.com/wcy1216/4006Info2020.git>
2020-10-05 11:38:18 +0800

    Lab1: Map and reset button

---
+++ 4006InfoDay2020/MapViewController.swift
@@ -0,0 +1,60 @@
+//
+//  MapViewController.swift
+//  4006InfoDay2020
+//
+//  Created by 17214726 on 5/10/2020.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+
+    // set initial location in HKBU
+    let campusLocation = CLLocation(latitude: 22.33787, longitude: 114.18131)
+    
+    override func viewDidLoad() {
+
+        mapView.setCenterLocation(campusLocation)
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func resetPosition(_ sender: Any) {
+        mapView.setCenterLocation(campusLocation)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+
+}
+
+private extension MKMapView {
+    
+    func setCenterLocation(_ location: CLLocation,
+                           regionRadius: CLLocationDistance = 500) {
+        
+        let coordinateRegion = MKCoordinateRegion(
+            center: location.coordinate,
+            latitudinalMeters: regionRadius,
+            longitudinalMeters: regionRadius)
+        
+        
+        
+        setRegion(coordinateRegion, animated: true)
+    }
+    
+}
