422b55290529a0c3ec462988895e8880506c491d
wcy1216 <https://github.com/wcy1216/4006Info2020.git>
2020-10-05 11:03:10 +0800

    Initial commit, fixed orientation

---
+++ 4006InfoDay2020/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4006InfoDay2020
+//
+//  Created by 17214726 on 5/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 4006InfoDay2020Tests/_006InfoDay2020Tests.swift
@@ -0,0 +1,33 @@
+//
+//  _006InfoDay2020Tests.swift
+//  4006InfoDay2020Tests
+//
+//  Created by 17214726 on 5/10/2020.
+//
+
+import XCTest
+@testable import _006InfoDay2020
+
+class _006InfoDay2020Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
